4e118dba299b96fb514ccd5b44d4b4a669bc137f
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2022-11-09 20:54:17 +0600

    requre express and cors and middle wares

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middle wares
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) =>{
+    res.send('LeadGen server is  running')
+})
