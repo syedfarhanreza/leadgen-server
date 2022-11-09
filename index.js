deeaa7c43339d836bb9840832e325b55a7b4ab48
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2022-11-09 21:41:39 +0600

    database connected

--- index.js
+++ index.js
@@ -14,11 +14,22 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0neovd0.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+    try{
+        const serviceCollection = client.db('photography').collection('services');
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.error(err))
 
 
 app.get('/', (req, res) =>{
